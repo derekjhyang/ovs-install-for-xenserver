c0953ea482e0c2a8909530153ee75b95614923f5
Winnie Yang <hswayne77@gmail.com>
2013-05-09 14:12:09 -0400

    add pip install fabric

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-./rpmbuild.sh 2>&1 | tee installation-log
+#./rpmbuild.sh 2>&1 | tee installation-log
+
+# install python fabric for bulk installation
+if [[ ! $(pip freeze | grep -i 'fabric') =~ '(f|F)abric' ]];then
+    pip install fabric
+    echo "[INFO] fabric is installed."
+fi
+
